b8da45d34f3fe425df60fbba3ff3e9b18543b76d
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-06-11 23:14:08 -0400

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,42 @@
-// create your App component here
+import react from "react";
+import { useEffect, useState } from "react";
+
+function App(){
+
+const[dogs, setDogs] = useState("")
+const[isLoading, setIsLoading] = useState(true)
+const[isDisplaying, setIsDisplaying] = useState(false)
+
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then ((object) => {
+        setIsLoading(false)
+        setDogs(object.message)
+        setIsDisplaying(true)
+        })
+
+}, [])
+
+function DisplayDog(){
+    if(isDisplaying === true) {
+        return <img src={dogs} alt="A Random Dog"></img>
+    } else {
+        return null;
+    }
+
+}
+
+return (
+    <div>
+        <p>{isLoading ? "Loading..." : null}</p>
+        <DisplayDog />
+        
+    </div>
+)
+
+}
+
+
+export default App;
